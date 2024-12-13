ca4563bf2f349a1d81aa08a827d5fb4a10b2a0e3
IshtiaqueMalik <166053815+IshtiaqueMalik@users.noreply.github.com>
2024-12-13 23:21:07 +0500

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,26 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import Hero from "./Components/Hero";
+import Brand from "./Components/Brand";
+import Ceramics from "./Components/Ceramics";
+import Product from "./Components/Product";
+import Benefit from "./Components/Benefit";
+import Touch from "./Components/Touch";
+import Productlisting from "./Productlisting/page";
+
+
+export default function Home() {
+  return (
+   <>
+   <Hero />
+   <Brand />
+   <Ceramics />
+   <Product />
+   <Benefit />
+   <Touch />
+   <Productlisting />
+
+   
+
+   </>
+  );
+}
